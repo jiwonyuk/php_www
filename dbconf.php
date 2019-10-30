ddffd216ff5149e3d16c9204155187dd83bc6caf
jiwonyuk <ykc4791@naver.com>
2019-10-30 11:30:03 +0900

    오토로드 설정

---
+++ dbconf.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$host = "localhost";
+$user = "phpuser";
+$passwd = "multi1004";
+$database = "php";
+
+
+return [ 
+    "host"=>$host, 
+    "user"=>$user,
+    "passwd"=>$passwd, 
+    "database"=>$database 
+];
