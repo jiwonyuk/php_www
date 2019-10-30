6c2ac0fef8fde3e06d3a433a72a4eff9588d0bd7
jiwonyuk <ykc4791@naver.com>
2019-10-30 16:20:25 +0900

    universal table

--- App/Controller/Tables.php
+++ App/Controller/Tables.php
@@ -20,7 +20,11 @@ class Tables
         $rows = []; // 배열 초기화
         for ($i=0;$i<$count;$i++) {
             $row = mysqli_fetch_object($result);
-            $rows []= $row; // 배열 추가
+            //$rows []= $row; // 배열 추가
+            $rows []=[
+              'num'=>$i,
+              'name'=>"<a href='/TableInfo/".$row->Tables_in_php."'>".$row->Tables_in_php."</a></td>"  
+            ];
         }
         $content = $html->table($rows);
         $body = file_get_contents("../Resource/table.html");
--- Module/Html/HtmlTable.php
+++ Module/Html/HtmlTable.php
@@ -7,17 +7,32 @@ class HtmlTable
     {
     $body = "<table class=\"table\">";
     $body .= "<thead>";
+    /*
     $body .= "<tr>
         <th>번호</th>
         <th>테이블명</th>
       </tr>";
+      */
+      $body .= "<tr>";
+      foreach ($rows[0] as $key =>$value) {
+        $body .= "<th>".$key."</th>";
+      }
+      $body .= "</tr>";
+
     $body .= "</thead>";
     $body .= "<tbody>";
     
+    // for = 상위배열
     for($i=0;$i<count($rows);$i++) {
         $body .= "<tr>";
-        $body .= "<td>$i</td>";
-        $body .= "<td><a href='/TableInfo/".$rows[$i]->Tables_in_php."'>".$rows[$i]->Tables_in_php."</a></td>";
+       // $body .= "<td>$i</td>";
+        //$body .= "<td><a href='/TableInfo/".$rows[$i]->Tables_in_php."'>".$rows[$i]->Tables_in_php."</a></td>";
+        // 하위배열
+        //키,값 연상배열 형태로 풀어서 작업함
+        foreach($rows[$i] as $key => $value) {
+            $body .= "<td>".$value."</td>";
+        }
+
         $body .= "</tr>";
     }    
     $body .= "</tbody>";
